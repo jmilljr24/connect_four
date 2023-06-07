38ff385a149c7826a6dd4c391ee65bf65dc7ed9b
jmilljr24 <jmilljr24@gmail.com>
2023-06-07 11:32:25 -0400

    Add horizontal check to win game

--- main.rb
+++ main.rb
@@ -15,6 +15,7 @@ end
 class Board
   include Miscellaneous
   attr_accessor :grid
+  attr_reader :winner
 
   def initialize
     @grid = Array.new(6) { Array.new(7) { empty_circle } }
@@ -26,11 +27,45 @@ class Board
     end
     puts (1..7).to_a.join(' ')
   end
+
+  def horizontal_check
+    @grid.each do |arr|
+      break unless winner.nil?
+
+      c = 0 # column of array
+      p1 = 0
+      p2 = 0
+      until p1 == 4 || p2 == 4 || c == 8
+        case arr[c]
+        when 'x'
+          p1 += 1
+        when 'y'
+          p2 += 1
+        end
+        c += 1
+      end
+      if p1 == 4
+        @winner = 'Player One'
+      elsif p2 == 4
+        @winner = 'Player Two'
+      end
+    end
+    return true unless @winner.nil?
+
+    false
+  end
+
+  def game_over?
+    return true unless @winner.nil?
+    return true unless @grid[0].include?(empty_circle)
+
+    false
+  end
 end
 
 board = Board.new
 
-board.display_board
+board.horizontal_check
 
 class User
   attr_reader :name, :symbol
@@ -57,14 +92,8 @@ class Game
   def play
     # until game_over?
   end
-
-  def game_over?
-    return false if @board.grid[0].include?(empty_circle)
-
-    true
-  end
 end
 
-game = Game.new
+# game = Game.new
 
-game.game_over?
+# game.win_vertical?
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,16 +1,36 @@
 require './main'
 
-describe Game do
+describe Board do
   context 'when checking if the game is over' do
     describe '#game_over?' do
-      subject(:game) { described_class.new }
+      subject(:board) { described_class.new }
       it 'returns false if the board is not full of selections' do
-        expect(game.game_over?).not_to eq true
+        expect(board.game_over?).not_to eq true
       end
 
       it 'returns true if the board is full' do
-        game.board.instance_variable_set(:@grid, Array.new(6) { Array.new(7) { 'x' } })
-        expect(game.game_over?).to eq true
+        board.instance_variable_set(:@grid, Array.new(6) { Array.new(7) { 'x' } })
+        expect(board.game_over?).to eq true
+      end
+    end
+
+    describe '#horizontal_check' do
+      subject(:horizontal_check) { described_class.new }
+      let(:grid) { horizontal_check.instance_variable_get(:@grid) }
+
+      before do
+        grid[0][1] = 'x'
+        grid[0][2] = 'x'
+        grid[0][3] = 'x'
+        grid[0][4] = 'x'
+      end
+      it 'returns true if a row has four pieces in a row' do
+        expect(horizontal_check.horizontal_check).to eq true
+      end
+
+      it 'returns false if there is not four pieces in a row in any row' do
+        grid[0][3] = 'y'
+        expect(horizontal_check.horizontal_check).to eq false
       end
     end
   end
